a79b1aca36f71a69494246a90c2c0c9db2ff9a0c
Park <rppr001@naver.com>
2021-08-14 14:58:48 +0900

    node middleware seventh commit

--- src/server.js
+++ src/server.js
@@ -4,16 +4,31 @@ const port = 4000;
 
 const app = express();
 
-const handleHome = (req, res) => {
-    return res.send("123");
+const logger =(req,res,next) => {
+console.log(`${req.method} ${req.url}`);
+    next();
 };
-const handleLogin =(req, res) => {
-    return res.send("login here.");
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
 
-handleHome
+const privateMiddleware =(req,res,next) =>{
+  const url = req.url;
+  if(url == "/protected"){
+    return res.send("<h1>Not Allrow</h1>");
+  }
+  console.log("go to home");
+  next();
+};
+
+const handleHome = (req, res) => {
+    return res.send("i love middleware");
+};
+
+const handleProtected =(req,res) =>{
+    return res.send("welcome to private logger");
+}
+app.use(logger);
+app.use(privateMiddleware);
+app.get("/protected", handleProtected);
+app.get("/",  handleHome);
 
 const handleListening = () => console.log(`server Listenting on port http://localhost:${port}`);
 
