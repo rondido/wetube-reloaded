1a8776eff6970cae8fd4e7bc72e76279f4698524
Park <rppr001@naver.com>
2021-08-19 20:16:30 +0900

    node.js express pug mixins sixteenth commit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,38 @@
-export const tranding = (req,res) => res.render("home", {pageTitle:"Home", potato:"tomato"});
+
+const fakeUser ={
+    username:"Park",
+    loggedIn:false,
+};
+
+export const tranding = (req,res) => {
+    const videos =[
+        {
+            title: "First Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 mintues ago",
+            views:59,
+            id:1
+        },
+        {
+            title: "secound Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 mintues ago",
+            views:59,
+            id:2
+        },
+        {
+            title: "third Video",
+            rating:5,
+            comments:2,
+            createdAt: "2 mintues ago",
+            views:59,
+            id:3
+        },
+    ];
+    return res.render("home", {pageTitle:"Home", videos});
+};
 export const see = (req,res) =>
      res.render("watch");
 export const edit = (req,res) =>{
