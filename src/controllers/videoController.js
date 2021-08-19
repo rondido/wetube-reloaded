e3d27b39cfbd88a6bfad2a67d1aaf0c2e79d923e
Park <rppr001@naver.com>
2021-08-19 16:34:04 +0900

    node.js express pug template variable rendering fifteenth commit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-export const tranding = (req,res) => res.render("home");
+export const tranding = (req,res) => res.render("home", {pageTitle:"Home", potato:"tomato"});
 export const see = (req,res) =>
      res.render("watch");
 export const edit = (req,res) =>{
